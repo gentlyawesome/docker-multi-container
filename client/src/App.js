e4360e02a9d866607eff6229781f6cd872d3492b
gentlyawesome <gentlyawesome@gmail.com>
2020-04-01 15:51:30 +0800

    fixed fib

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
-import Fib from './Fib';
+import Fib from './Fib'
 
 class App extends Component {
   render() {
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    let data = Array.from(this.state.seenIndexes);
+    return data.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
